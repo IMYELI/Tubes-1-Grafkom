faacd9639d88a0ed68198f22596479e04aad9dd8
IMYELI <13520091@std.stei.itb.ac.id>
2023-02-08 21:12:35 +0700

    Setup: setting up the environment to draw simple rectangles

---
+++ src/main.js
@@ -0,0 +1,78 @@
+// Generate an integer from 0 to (range-1)
+function randInt(range) {
+  return Math.floor(Math.random() * range);
+}
+
+//Buffering the points of rectangle.
+function bufferRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW);
+}
+
+// Setting the color of object
+function bufferRandomColor(gl, colorUniformLocation, opacity = 1) {
+  gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), opacity);
+}
+
+// Create <count> rectangle
+function generateRectangle(gl, colorUniformLocation, count = 1) {
+  var primitiveType = gl.TRIANGLES;
+  var offset = 0;
+  var shaderCount = 6;
+  for (var i = 0; i < count; i++) {
+    bufferRectangle(gl, randInt(300), randInt(300), randInt(300), randInt(300));
+    bufferRandomColor(gl, colorUniformLocation);
+    gl.drawArrays(primitiveType, offset, shaderCount);
+  }
+}
+
+async function main() {
+  var webglUtil = new webglUtils();
+  var canvas = document.querySelector("#canvas");
+  var gl = canvas.getContext("webgl");
+  if (!gl) {
+    window.alert("Error initializing WebGL");
+    return;
+  }
+
+  var program = await webglUtil.createDefaultProgram(gl);
+  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  var positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  webglUtil.resizeCanvasToDisplaySize(gl.canvas);
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Using the default program
+  gl.useProgram(program);
+
+  // enabling the attribute so that we can take data from the buffer
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+  var size = 2; // 2 components per iteration
+  var type = gl.FLOAT; // the data is 32bit floats
+  var normalize = false; // don't normalize the data
+  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
+  var offset = 0; // start at the beginning of the buffer
+  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+  // Setting the resolution
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+  generateRectangle(gl, colorUniformLocation, 5);
+}
+
+main();
---
+++ src/utils/webglUtils.js
@@ -0,0 +1,84 @@
+class webglUtils {
+  constructor() {
+    console.log("Tes");
+  }
+  /*
+   *   Create and compile a shader.
+   *
+   *   @param gl → webGL context;
+   *   @param source → GLSL source
+   *   @param type → the type of shader (VERTEX_SHADER or FRAGMENT_SHADER)
+   *   @return the shader
+   */
+  createShader(gl, source, type) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    var isSuccess = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (!isSuccess) {
+      throw "Could not compile shader: " + gl.getShaderInfoLog(shader);
+    }
+    return shader;
+  }
+
+  /*
+   *    create a program that is linked with vertexShader and fragmentShader
+   *
+   *    @param gl → webGL context;
+   *    @param vertexShader → a vertex shader
+   *    @param framgentShader → a fragment shader
+   *    @return a program
+   */
+  async createProgram(gl, vertexShaderSource, fragmentShaderSource) {
+    var program = gl.createProgram();
+    var vertexShader = this.createShader(gl, document.getElementById(vertexShaderSource).text, gl.VERTEX_SHADER);
+    var fragmentShader = this.createShader(gl, document.getElementById(fragmentShaderSource).text, gl.FRAGMENT_SHADER);
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    var isSuccess = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (!isSuccess) {
+      gl.deleteProgram(program);
+      throw "Failed linking program: " + gl.getProgramInfoLog(program);
+    }
+    return program;
+  }
+
+  /*
+   *  creating a program using only gl parameter
+   *
+   *  @param gl → WebGL context;
+   *  @return program
+   */
+  async createDefaultProgram(gl) {
+    var program = await this.createProgram(gl, "vertexShader", "fragmentShader");
+    return program;
+  }
+
+  /*
+   *  Function to make our canvas the same size with the pixels that is drawn
+   *
+   *
+   *  @param canvas → our canvas that is used to render the pixels.
+   */
+  resizeCanvasToDisplaySize(canvas) {
+    // Lookup the size the browser is displaying the canvas in CSS pixels.
+    const displayWidth = canvas.clientWidth;
+    const displayHeight = canvas.clientHeight;
+
+    // Check if the canvas is not the same size.
+    const needResize = canvas.width !== displayWidth || canvas.height !== displayHeight;
+
+    if (needResize) {
+      // Make the canvas the same size
+      canvas.width = displayWidth;
+      canvas.height = displayHeight;
+    }
+
+    return needResize;
+  }
+}
